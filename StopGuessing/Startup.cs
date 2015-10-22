6182c72bc5d69d5c24669880e4a8b4dcabd8f15a
Stuart Schechter <stuart.schechter@microsoft.com>
2015-10-22 09:07:15 -0700

    Publish to Azure now seems to work.

--- StopGuessing/Startup.cs
+++ StopGuessing/Startup.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.Dnx.Runtime;
