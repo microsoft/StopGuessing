e730e415ee2536f8aac8bf62e1c231cd1c9c8016
Stuart Schechter <stuart.schechter@outlook.com>
2015-11-08 08:52:56 -0800

    Added, but have not tested, a new parallelization approach.

--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -81,10 +81,75 @@ namespace Simulator
             StableStore.LoginAttempts = null;
         }
 
+        public static async Task ParallelRepeat(
+            ulong numberOfTimesToRepeat,
+            Action actionToRun,
+            Action<Exception> callOnException = null,
+            int maxConcurrentTasks = 1000)
+        {
+            Task[] activeTasks = new Task[maxConcurrentTasks];
+            Dictionary<Task, int> taskToIndex = new Dictionary<Task, int>();
+            HashSet<Task> exceptionHandlingTasks = new HashSet<Task>();
+
+            ulong tasksStarted = 0;
+            // Phase 1 -- start maxConcurrentTasks executing
+            while (tasksStarted < (ulong) activeTasks.Length && tasksStarted < numberOfTimesToRepeat)
+            {
+                Task startedTask = Task.Run(actionToRun);
+                activeTasks[tasksStarted] = startedTask;
+                taskToIndex[startedTask] = (int) tasksStarted;
+                tasksStarted++;
+            }
+
+            // Phase 2 -- A stable stat in which there are always the maximum number of tasks
+            //            in our array of active tasks
+            while (tasksStarted < numberOfTimesToRepeat)
+            {
+                // Wait for a task to complete
+                Task completedTask = await Task.WhenAny(activeTasks.ToArray());
+                int indexOfTaskToReplace = taskToIndex[completedTask];
+                // Replace the task that completed with a new task...
+                // If there was an exception, the replacement should be a task to handle
+                // that exception.  Otherwise, it should be the next work item.
+                bool callExceptionHandler = false;
+                if (callOnException != null)
+                {
+                    bool completedTaskWasExceptionHandler = exceptionHandlingTasks.Contains(completedTask);
+                    // We'll want to run a task with the caller's exception handler...
+                    callExceptionHandler = completedTask.IsFaulted;
+                    if (completedTaskWasExceptionHandler)
+                    {
+                        // unless it was the caller's exception handler that faulted
+                        exceptionHandlingTasks.Remove(completedTask);
+                        callExceptionHandler = false;
+                    }
+                }
+                Task replacementTask = callExceptionHandler
+                    ? Task.Run(() => callOnException(completedTask.Exception))
+                    : Task.Run(actionToRun);
+                // Put the replacement task at the same index in the array as the prior task
+                activeTasks[indexOfTaskToReplace] = replacementTask;
+                taskToIndex.Remove(completedTask);
+                taskToIndex[replacementTask] = indexOfTaskToReplace;
+                tasksStarted++;
+            }
+
+            // Phase 3 -- A final phase in which we empty out exceptions
+            Task.WaitAll(activeTasks);
+            if (callOnException != null)
+            {
+                foreach (Task exceptionTask in activeTasks.Where(t => t.IsFaulted))
+                    #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                    Task.Run(() => callOnException(exceptionTask.Exception));
+            }
+        }
+
 
 
         public static async Task RunWithMaxDegreeOfConcurrency<T>(
-   int maxDegreeOfConcurrency, IEnumerable<T> collection, Func<T, Task> taskFactory)
+            int maxDegreeOfConcurrency,
+            IEnumerable<T> collection, 
+            Func<T, Task> taskFactory)
         {
             var activeTasks = new List<Task>(maxDegreeOfConcurrency);
             foreach (var task in collection.Select(taskFactory))
